e1514e79378aed6ae0f3fd980ea589ba8cc88f67
Samriddh Negi <samridh.negi007@gmail.com>
2024-08-31 16:39:23 +0530

    connected mongo Atlas Service

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,23 @@
 const express = require('express')
+const env = require('dotenv/config')
+const {connectToMongo} = require('./connect.js')
 
+const app = express()
+
+//connect mongoDB
+connectToMongo()
+.then(() => {console.log('MongoDB connected')})
+.catch((err) => {console.log('Error : ', err)})
+
+//Middlewares
+
+
+//Routes
+app.get('/', (req, res) =>{
+    res.send('Hello')
+})
+
+
+app.listen(process.env.PORT, () =>{
+    console.log('listening on Port : ', process.env.PORT)
+})
